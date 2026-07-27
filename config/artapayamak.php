dcd9bc91b6a533bfaff623645c50a46dcd0da71c
artapayamak <faridamani67@gmail.com>
2026-07-27 16:42:52 +0330

    Update artapayamak.php

--- config/artapayamak.php
+++ config/artapayamak.php
@@ -1,1 +1,16 @@
+<?php
 
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | تنظیمات آرتا پیامک
+    |--------------------------------------------------------------------------
+    |
+    | Here you can specify your IPPanel API key and optionally override the base URL.
+    |
+    */
+
+    'api_key' => env('ARTAPAYAMAK_API_KEY', ''),
+
+    'base_url' => env('ARTAPAYAMAK_BASE_URL', 'https://edge.ippanel.com/v1/api'),
+];
